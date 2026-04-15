2ad6bf53d294416857fed8e0ee31a1c84cd8e3d4
Humanoid-Human <121205339+Humanoid-Human@users.noreply.github.com>
2026-04-15 11:45:05 -0400

    suggestions from review
    - arbitrary number of arguments
    - `set -eu`
    - traps to clean up temporary files/dirs
    - no cd into tempdir

--- pkgdist.sh
+++ pkgdist.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# usage: ./pkgdist.sh <option>
-# <option> is one of:
+# usage: ./pkgdist.sh [<option>]
+# <option> is any of:
 # 	- a pkg file, with all dependencies in the same location
 # 		(can be local, http://, https://, or ftp://)
 #   - 'src'
@@ -14,15 +14,22 @@
 #   - 'minimal'
 #   - 'minimal-dbg'
 
+if [ -z "$1" ]; then
+	echo "Expected 1 or more arguments, got 0" >&2
+	exit 1;
+fi
+
 tar_xz="tar -cvJf"
-code=0
-input_name="${1}"
+
+set -eu
+trap '[ -e "${temp}" ] && rm -rf "${temp}"' EXIT
 
 get_files() {
-	temp="${1}"; shift
-	package="${1}"; shift
-	pkglist="${1}"; shift
-	skip_first="${1}"
+	temp="$1"; shift
+	package="$1"; shift
+	pkglist="$1"; shift
+	skip_first="$1"
+
 	for dep in $(pkg info -dq "${package}*"); do
 		for old_dep in ${pkglist}; do
 			if [ "${old_dep}" = "${dep}" ]; then
@@ -37,58 +44,53 @@ get_files() {
 	fi
 }
 
-case ${input_name} in
-	src|lib32|base-dbg|kernels-dbg|minimal|minimal-dbg)
-		temp=$(mktemp) || exit 1
-		get_files "${temp}" "FreeBSD-set-${input_name}"
-		${tar_xz} "${input_name}.txz" -nLT "${temp}"
-		code=$?
-		rm "${temp}"
-		;;
+while [ -n "$1" ]; do
+	input_name="$1"; shift
+	case ${input_name} in
+		src|lib32|base-dbg|kernels-dbg|minimal|minimal-dbg)
+			temp=$(mktemp) || exit 1
+			get_files "${temp}" "FreeBSD-set-${input_name}"
+			${tar_xz} "${input_name}.txz" -nLT "${temp}"
+			rm "${temp}"
+			;;
 
-	ports)
-		${tar_xz} ports.txz -L \
-		--exclude .svn --exclude .git --exclude distfiles \
-		--exclude packages --exclude INDEX* --exclude work \
-		/usr/ports
-		code=$?
-		;;
+		ports)
+			${tar_xz} ports.txz -L \
+			--exclude .svn --exclude .git --exclude distfiles \
+			--exclude packages --exclude INDEX* --exclude work \
+			/usr/ports
+			;;
 
-	kernel)
-		kern=$(pkg info -x FreeBSD-kernel | head -n 1)
-		echo Making dist set from "${kern}"
-		temp=$(mktemp) || exit 1
-		get_files "${temp}" "${kern}" 
-		${tar_xz} kernel.txz -T "${temp}"
-		code=$?
-		rm "${temp}"
-		;;
+		kernel)
+			kern=$(pkg info -x FreeBSD-kernel | head -n 1)
+			echo Making dist set from "${kern}"
+			temp=$(mktemp) || exit 1
+			get_files "${temp}" "${kern}" 
+			${tar_xz} kernel.txz -T "${temp}"
+			rm "${temp}"
+			;;
 
-	base)
-		temp=$(mktemp) || exit 1
-		get_files "${temp}" FreeBSD-set-base
-		${tar_xz} base.txz -T "${temp}"
-		code=$?
-		rm "${temp}"
-		;;
+		base)
+			temp=$(mktemp) || exit 1
+			get_files "${temp}" FreeBSD-set-base
+			${tar_xz} base.txz -T "${temp}"
+			rm "${temp}"
+			;;
 
-	*)
-		# install the pkg to a temp folder and package that.
-		tempdir=$(mktemp -d) || exit 1
-		curdir=$(pwd)
-		cd "${tempdir}" || exit 1
-		INSTALL_AS_USER=true ASSUME_ALWAYS_YES=true pkg -r . add "${curdir}/${input_name}"
+		*) # install the pkg to a temp folder and package that.
+			temp=$(mktemp -d) || exit 1
 
-		# remove the pkg-generated files
-		rm -r ./var/db/pkg/* ./var/db/pkg/.[!.]*
-		[ -n "$(ls -A ./var/db/pkg || :)" ] || rm -r ./var/db/pkg
-		[ -n "$(ls -A ./var/db || :)" ] || rm -r ./var/db
-		[ -n "$(ls -A ./var || :)" ] || rm -r ./var
-		
-		# make tarball
-		find . | sed 1d | ${tar_xz} "${curdir}/${input_name%pkg}txz" -nT -
-		cd "${curdir}" && rm -r "${tempdir}"
-		;;
-esac
+			INSTALL_AS_USER=true ASSUME_ALWAYS_YES=true pkg -r "${temp}" add "${input_name}"
 
-exit "${code}"
+			# remove the pkg-generated files
+			rm -r ./var/db/pkg/* ./var/db/pkg/.[!.]* || :
+			[ -z "$(ls -A ./var/db/pkg || :)" ] && rm -r ./var/db/pkg
+			[ -z "$(ls -A ./var/db || :)" ] && rm -r ./var/db
+			[ -z "$(ls -A ./var || :)" ] && rm -r ./var
+			
+			# make tarball
+			find "${temp}" | sed 1d | ${tar_xz} "${input_name%pkg}txz" -nLT -
+			rm -r "${temp}"
+			;;
+	esac
+done
